35bd3528ff668c6ce98209f8dd119dd512cd4970
sukeshnibakshi29 <82715100+sukeshnibakshi29@users.noreply.github.com>
2021-05-11 21:45:37 -1200

    Update Rope.js

--- Rope.js
+++ Rope.js
@@ -3,7 +3,7 @@ class Rope{
         var options = {
             bodyA: bodyA,
             bodyB: bodyB,
-  
+            stiffness:1
             length: 10
         }
         this.chain = Constraint.create(options);
